b17ee0b4b3cc57c4d22a787e15fb161657a7bbd6
pixaily <danield.georgiev@gmail.com>
2026-01-07 08:40:23 +0200

    Fix styling

--- challenges/01-array.php
+++ challenges/01-array.php
@@ -8,116 +8,129 @@
   <title>PHP From Scrach - Array Challenges</title>
 </head>
 
-<body>
-  <h2>Array Challenges</h2>
+<body class="bg-gray-100">
 
-  <p><a href="/tasks/20-array-challenges.md" target="_blank">Tasks</a></p>
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl mb-2">Array Challenges</h1>
+    </div>
+  </header>
 
-  <h3>Challenge 1: Sum of an array</h3>
+  <div class="container mx-auto mt-4">
+    <div class="p-4">
+      <p class="mb-2"><a href="/tasks/20-array-challenges.md" target="_blank" class="text-blue">Tasks readme</a></p>
 
-  <p>Create an array of numbers and get the sum without using array_sum(). Then print out 'The sum of the {amount}
-    numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be The sum of the 5 numbers is:
-    15.</p>
+      <h2 class="text-xl mb-2">Challenge 1: Sum of an array</h2>
 
-  <?php
-  $numbers = [1, 2, 3, 4, 5];
-  $sum = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
+      <p>Create an array of numbers and get the sum without using array_sum(). Then print out 'The sum of the {amount}
+        numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be The sum of the 5 numbers
+        is:
+        15.</p>
 
-  echo "<p>The sum of the numbers is: $sum</p>";
-  ?>
+      <?php
+      $numbers = [1, 2, 3, 4, 5];
+      $sum = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
 
-  <hr>
+      echo "<p>The sum of the numbers is: $sum</p>";
+      ?>
+    </div>
+    <hr>
+    <div class="p-4 mt-4">
+      <h3>Challenge 2: Colors array</h3>
 
-  <h3>Challenge 2: Colors array</h3>
+      <p>
+        $colors = ['red', 'blue', 'green', 'yellow'];
+      <ul>
+        <li>Reverse the $colors array</li>
+        <li>Add 'purple' and 'orange' to the end of the array</li>
+        <li>Add the color 'pink' as the second color in the array.</li>
+        <li>Remove the last element of the array</li>
+      </ul>
+      You should end up with the following array:
 
-  <p>
-    $colors = ['red', 'blue', 'green', 'yellow'];
-  <ul>
-    <li>Reverse the $colors array</li>
-    <li>Add 'purple' and 'orange' to the end of the array</li>
-    <li>Add the color 'pink' as the second color in the array.</li>
-    <li>Remove the last element of the array</li>
-  </ul>
-  You should end up with the following array:
+      <code>
+        ['yellow', 'pink', 'green', 'blue', 'red', 'purple'];
+      </code>
+      </p>
 
-  <code>
-    ['yellow', 'pink', 'green', 'blue', 'red', 'purple'];
-  </code>
-  </p>
+      <?php
+      $colors = ['red', 'blue', 'green', 'yellow'];
 
-  <?php
-  $colors = ['red', 'blue', 'green', 'yellow'];
+      $colors = array_reverse($colors);
 
-  $colors = array_reverse($colors);
+      array_push($colors, 'purple');
 
-  array_push($colors, 'purple');
+      $colors = array_merge($colors, ['orange']);
 
-  $colors = array_merge($colors, ['orange']);
+      array_splice($colors, 1, 0, 'pink');
 
-  array_splice($colors, 1, 0, 'pink');
+      array_pop($colors);
 
-  array_pop($colors);
+      print_r($colors)
+      ?>
+    </div>
 
-  print_r($colors)
-  ?>
+    <hr>
 
-  <hr>
+    <div class="p-4 mt-4">
+      <h3>Challenge 3: Job Listing Array</h3>
 
-  <h3>Challenge 3: Job Listing Array</h3>
+      <ol>
+        <li> Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title,
+          company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an
+          array
+          of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.</li>
+        <li>2. Create a new listing using the `array_push()` function. The new listing should have the same fields as
+          the
+          others.</li>
+        <li>3. Print out the job_title of the second job listing in the array.</li>
+        <li>4. Print out the first skill of the third job listing in the array.</li>
+      </ol>
 
-  <ol>
-    <li> Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title,
-      company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an array
-      of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.</li>
-    <li>2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the
-      others.</li>
-    <li>3. Print out the job_title of the second job listing in the array.</li>
-    <li>4. Print out the first skill of the third job listing in the array.</li>
-  </ol>
+      <?php
+      $job_listings = [
+        [
+          'id' => 1,
+          'job_title' => 'PHP Developer',
+          'company' => 'ABC Company',
+          'contact_email' => 'john@email.com',
+          'contact_phone' => '123-456-7890',
+          'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS']
+        ],
+        [
+          'id' => 2,
+          'job_title' => 'Web Designer',
+          'company' => 'XYZ Company',
+          'contact_email' => 'steph@email.com',
+          'contact_phone' => '123-456-7890',
+          'skills' => ['Photoshop', 'Illustrator', 'HTML', 'CSS']
+        ],
+        [
+          'id' => 3,
+          'job_title' => 'Web Developer',
+          'company' => '123 Company',
+          'contact_email' => 'peter@email.com',
+          'contact_phone' => '123-456-7890',
+          'skills' => ['Python', 'PostgreSQL', 'JavaScript', 'HTML', 'CSS']
+        ]
+      ];
 
-  <?php
-  $job_listings = [
-    [
-      'id' => 1,
-      'job_title' => 'PHP Developer',
-      'company' => 'ABC Company',
-      'contact_email' => 'john@email.com',
-      'contact_phone' => '123-456-7890',
-      'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS']
-    ],
-    [
-      'id' => 2,
-      'job_title' => 'Web Designer',
-      'company' => 'XYZ Company',
-      'contact_email' => 'steph@email.com',
-      'contact_phone' => '123-456-7890',
-      'skills' => ['Photoshop', 'Illustrator', 'HTML', 'CSS']
-    ],
-    [
-      'id' => 3,
-      'job_title' => 'Web Developer',
-      'company' => '123 Company',
-      'contact_email' => 'peter@email.com',
-      'contact_phone' => '123-456-7890',
-      'skills' => ['Python', 'PostgreSQL', 'JavaScript', 'HTML', 'CSS']
-    ]
-  ];
+      array_push($job_listings, [
+        'id' => 4,
+        'job_title' => 'Full Stack Developer',
+        'company' => '456 Company',
+        'contact_email' => 'jane@email.com',
+        'contact_phone' => '123-456-7890',
+        'skills' => ['Node.js', 'MongoDB', 'React', 'HTML', 'CSS']
+      ]);
 
-  array_push($job_listings, [
-    'id' => 4,
-    'job_title' => 'Full Stack Developer',
-    'company' => '456 Company',
-    'contact_email' => 'jane@email.com',
-    'contact_phone' => '123-456-7890',
-    'skills' => ['Node.js', 'MongoDB', 'React', 'HTML', 'CSS']
-  ]);
+      echo "<p>The job title of the second listing is: " . $job_listings[1]['job_title'] . "</p>";
 
-  echo "<p>The job title of the second listing is: " . $job_listings[1]['job_title'] . "</p>";
+      echo "<p>The first skill of the third listing is: " . $job_listings[2]['skills'][0] . "</p>";
 
-  echo "<p>The first skill of the third listing is: " . $job_listings[2]['skills'][0] . "</p>";
-
-  ?>
-
+      ?>
+    </div>
+  </div>
 </body>
 
 </html>
--- challenges/challenges.php
+++ challenges/challenges.php
@@ -8,118 +8,125 @@
   <title>PHP From Scrach - Challenges</title>
 </head>
 
-<body>
-  <h1>PHP From Scratch - Challenges</h1>`
+<body class="bg-gray-100">
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <h1 class="text-3xl mb-2">Multi-dimensional Challenges</h1>
+    </div>
+  </header>
 
-  <h2>Array Challenges</h2>
+  <div class="container mx-auto p-4 mt-4">
+    <h2 class="font-semibold">Challenge: Loop Job Listings</h2>
 
-  <p><a href="/challenges/array-challenges.md" target="_blank">Tasks</a></p>
+    <p><a href="/challenges/array-challenges.md" target="_blank">Tasks</a></p>
 
-  <h3>Challenge 1: Sum of an array</h3>
+    <h3>Challenge 1: Sum of an array</h3>
 
-  <p>Create an array of numbers and get the sum without using array_sum(). Then print out 'The sum of the {amount}
-    numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be The sum of the 5 numbers is:
-    15.</p>
+    <p>Create an array of numbers and get the sum without using array_sum(). Then print out 'The sum of the {amount}
+      numbers is: {sum} '. For example, if the array is [1, 2, 3, 4, 5], the output should be The sum of the 5 numbers
+      is:
+      15.</p>
 
-  <?php
-  $numbers = [1, 2, 3, 4, 5];
-  $sum = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
+    <?php
+    $numbers = [1, 2, 3, 4, 5];
+    $sum = $numbers[0] + $numbers[1] + $numbers[2] + $numbers[3] + $numbers[4];
 
-  echo "<p>The sum of the numbers is: $sum</p>";
-  ?>
+    echo "<p>The sum of the numbers is: $sum</p>";
+    ?>
 
-  <hr>
+    <hr>
 
-  <h3>Challenge 2: Colors array</h3>
+    <h3>Challenge 2: Colors array</h3>
 
-  <p>
+    <p>
+      $colors = ['red', 'blue', 'green', 'yellow'];
+    <ul>
+      <li>Reverse the $colors array</li>
+      <li>Add 'purple' and 'orange' to the end of the array</li>
+      <li>Add the color 'pink' as the second color in the array.</li>
+      <li>Remove the last element of the array</li>
+    </ul>
+    You should end up with the following array:
+
+    <code>
+      ['yellow', 'pink', 'green', 'blue', 'red', 'purple'];
+    </code>
+    </p>
+
+    <?php
     $colors = ['red', 'blue', 'green', 'yellow'];
-  <ul>
-    <li>Reverse the $colors array</li>
-    <li>Add 'purple' and 'orange' to the end of the array</li>
-    <li>Add the color 'pink' as the second color in the array.</li>
-    <li>Remove the last element of the array</li>
-  </ul>
-  You should end up with the following array:
 
-  <code>
-    ['yellow', 'pink', 'green', 'blue', 'red', 'purple'];
-  </code>
-  </p>
+    $colors = array_reverse($colors);
 
-  <?php
-  $colors = ['red', 'blue', 'green', 'yellow'];
+    array_push($colors, 'purple');
 
-  $colors = array_reverse($colors);
+    $colors = array_merge($colors, ['orange']);
 
-  array_push($colors, 'purple');
+    array_splice($colors, 1, 0, 'pink');
 
-  $colors = array_merge($colors, ['orange']);
+    array_pop($colors);
 
-  array_splice($colors, 1, 0, 'pink');
+    print_r($colors)
+    ?>
 
-  array_pop($colors);
+    <hr>
 
-  print_r($colors)
-  ?>
+    <h3>Challenge 3: Job Listing Array</h3>
 
-  <hr>
+    <ol>
+      <li> Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title,
+        company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an
+        array
+        of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.</li>
+      <li>2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the
+        others.</li>
+      <li>3. Print out the job_title of the second job listing in the array.</li>
+      <li>4. Print out the first skill of the third job listing in the array.</li>
+    </ol>
 
-  <h3>Challenge 3: Job Listing Array</h3>
+    <?php
+    $job_listings = [
+      [
+        'id' => 1,
+        'job_title' => 'PHP Developer',
+        'company' => 'ABC Company',
+        'contact_email' => 'john@email.com',
+        'contact_phone' => '123-456-7890',
+        'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS']
+      ],
+      [
+        'id' => 2,
+        'job_title' => 'Web Designer',
+        'company' => 'XYZ Company',
+        'contact_email' => 'steph@email.com',
+        'contact_phone' => '123-456-7890',
+        'skills' => ['Photoshop', 'Illustrator', 'HTML', 'CSS']
+      ],
+      [
+        'id' => 3,
+        'job_title' => 'Web Developer',
+        'company' => '123 Company',
+        'contact_email' => 'peter@email.com',
+        'contact_phone' => '123-456-7890',
+        'skills' => ['Python', 'PostgreSQL', 'JavaScript', 'HTML', 'CSS']
+      ]
+    ];
 
-  <ol>
-    <li> Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title,
-      company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an array
-      of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.</li>
-    <li>2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the
-      others.</li>
-    <li>3. Print out the job_title of the second job listing in the array.</li>
-    <li>4. Print out the first skill of the third job listing in the array.</li>
-  </ol>
-
-  <?php
-  $job_listings = [
-    [
-      'id' => 1,
-      'job_title' => 'PHP Developer',
-      'company' => 'ABC Company',
-      'contact_email' => 'john@email.com',
+    array_push($job_listings, [
+      'id' => 4,
+      'job_title' => 'Full Stack Developer',
+      'company' => '456 Company',
+      'contact_email' => 'jane@email.com',
       'contact_phone' => '123-456-7890',
-      'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS']
-    ],
-    [
-      'id' => 2,
-      'job_title' => 'Web Designer',
-      'company' => 'XYZ Company',
-      'contact_email' => 'steph@email.com',
-      'contact_phone' => '123-456-7890',
-      'skills' => ['Photoshop', 'Illustrator', 'HTML', 'CSS']
-    ],
-    [
-      'id' => 3,
-      'job_title' => 'Web Developer',
-      'company' => '123 Company',
-      'contact_email' => 'peter@email.com',
-      'contact_phone' => '123-456-7890',
-      'skills' => ['Python', 'PostgreSQL', 'JavaScript', 'HTML', 'CSS']
-    ]
-  ];
+      'skills' => ['Node.js', 'MongoDB', 'React', 'HTML', 'CSS']
+    ]);
 
-  array_push($job_listings, [
-    'id' => 4,
-    'job_title' => 'Full Stack Developer',
-    'company' => '456 Company',
-    'contact_email' => 'jane@email.com',
-    'contact_phone' => '123-456-7890',
-    'skills' => ['Node.js', 'MongoDB', 'React', 'HTML', 'CSS']
-  ]);
+    echo "<p>The job title of the second listing is: " . $job_listings[1]['job_title'] . "</p>";
 
-  echo "<p>The job title of the second listing is: " . $job_listings[1]['job_title'] . "</p>";
+    echo "<p>The first skill of the third listing is: " . $job_listings[2]['skills'][0] . "</p>";
 
-  echo "<p>The first skill of the third listing is: " . $job_listings[2]['skills'][0] . "</p>";
-
-  ?>
-
+    ?>
+  </div>
 </body>
 
 </html>
